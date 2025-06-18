539792e7134a5d1b072b9d57489f4e181f425108
Shabazz Abdulrahman <Shabazz.Abdulrahman@digital.homeoffice.gov.uk>
2025-06-18 20:30:35 +0100

    rendering step function definition as template

--- modules/appstream2-automation-infra/main.tf
+++ modules/appstream2-automation-infra/main.tf
@@ -146,5 +146,11 @@ resource "aws_ssm_document" "appstream_setup" {
 resource "aws_sfn_state_machine" "appstream_automation" {
   name       = "${var.project_name}-state-machine"
   role_arn   = aws_iam_role.step_function_role.arn
-  definition = file(var.stepfn_definition_file)
+  # definition = file(var.stepfn_definition_file)
+  definition = templatefile(
+    var.stepfn_definition_file,
+    {
+      SSMDocName = aws_ssm_document.appstream_setup.name
+    }
+  )
 }
